1425ceaf61de4d802ce0aa8f8c0f9e26e4a96b72
FarelSaefulFalah <SaefulFalahFarel@gmail.com>
2024-08-01 14:14:23 +0700

    News

--- app/Http/Controllers/Api/BeritaController.php
+++ app/Http/Controllers/Api/BeritaController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Berita;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
-
+use Storage;
 class BeritaController extends Controller
 {
     public function index()
@@ -40,7 +40,7 @@ class BeritaController extends Controller
         }
 
         try {
-            $path = $request->file('foto')->store('berita');
+            $path = $request->file('foto')->store('public/berita');
             $berita = new Berita;
             $berita->judul = $request->judul;
             $berita->slug = $request->slug ?? Str::slug($request->judul);
@@ -67,16 +67,88 @@ class BeritaController extends Controller
 
     public function show(string $id)
     {
-        // Implement the show method
+        try {
+            $berita = Berita::findOrFail($id);
+            return response()->json([
+                'success' => true,
+                'message' => 'Berita berhasil ditampilkan',
+                'data' => $berita,
+            ], 200);
+        } catch (\Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'cumthing werong bby',
+                'data' => $berita,
+            ], 404);
+        }
     }
 
     public function update(Request $request, string $id)
     {
-        // Implement the update method
+        $validator = Validator::make($request->all(), [
+            'judul' => 'required',
+            'deskripsi' => 'required',
+            'foto' => 'nullable|image|mimes:png,jpg|max:2048',
+            'id_kategori' => 'required',
+            'tag' => 'required|array',
+            'id_user' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validasi gagal',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        try {
+            $berita = Berita::findOrFail($id);
+            if ($request->hasFile('foto')){
+                Storage::delete($berita->foto);
+                $path = $request->file('foto')->store('berita');
+                $berita->foto = $path;
+            }
+            $path = $request->file('foto')->store('public/berita');
+            $berita->judul = $request->judul;
+            $berita->slug = $request->slug ?? Str::slug($request->judul);
+            $berita->deskripsi = $request->deskripsi;
+            $berita->foto = $path;
+            $berita->id_kategori = $request->id_kategori;
+            $berita->id_user = $request->id_user;
+            $berita->save();
+
+            $berita->tag()->sync($request->tag);
+            return response()->json([
+                'success' => true,
+                'message' => 'Berita berhasil diperbarui',
+                'data' => $berita,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalahan',
+                'errors' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     public function destroy(string $id)
     {
-        // Implement the destroy method
+        try {
+            $berita = Berita::findOrFail($id);
+            $berita->tag()->detach();
+            Storage::delete($berita->foto);
+            return response()->json([
+                'success' => true,
+                'message' => 'Berita'.$berita->judul. ' berhasil ditampilkan',
+            ], 200);
+        } catch (\Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'cumthing werong bby',
+                'erorrs' => $e->getMessage(),
+            ], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,29 +6,30 @@ use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\TagController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\BeritaController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These;
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user/profile', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('kategori', [KategoriController::class, 'index']);
-// Route::post('kategori', [KategoriController::class, 'store']);
-// Route::get('kategori/{id}', [KategoriController::class, 'show']);
-// Route::put('kategori/{id}', [KategoriController::class, 'update']);
-// Route::delete('kategori/{id}', [KategoriController::class, 'delete']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-route::resource('kategori', KategoriController::class)->except(['edit', 'create']);
-route::resource('tag', TagController::class)->except(['edit', 'create']);
-route::resource('user', UserController::class)->except(['edit', 'create']);
-route::resource('berita', BeritaController::class)->except(['edit', 'create']);
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::resource('kategori', KategoriController::class)->except(['edit', 'create']);
+    Route::resource('tag', TagController::class)->except(['edit', 'create']);
+    Route::resource('user', UserController::class)->except(['edit', 'create']);
+    Route::resource('berita', BeritaController::class)->except(['edit', 'create']);
+});
